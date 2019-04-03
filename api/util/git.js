a946adc00aa1a17b2b4b4217f0fe87f79a26de12
portree_kid <keith.paterson@gmx.de>
2019-04-03 08:34:29 +0200

    Added error handling

--- api/util/git.js
+++ api/util/git.js
@@ -63,7 +63,7 @@ module.exports.clone2 = function (localPath, name, email, saveFunction, errCb, o
 	// Once the repository has been cloned or opened, you can work with a
 	// returned
 	// `Git.Repository` instance.
-	cloneRepository.catch(errorAndAttemptOpen.catch(errCb))
+	cloneRepository.catch(errorAndAttemptOpen).catch(errCb)
 		.then(function (repository) {
 			console.log("Got Repository");
 			// Access any repository methods here.
