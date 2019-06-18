d4c91d71aa902438a1561178cfe0738f81af8b9c
portree_kid <keith.paterson@gmx.de>
2019-06-18 07:59:54 +0200

    Tests

--- api/util/git.js
+++ api/util/git.js
@@ -1,7 +1,6 @@
 var NodeGit = require("nodegit");
 var path = require("path");
 var upath = require("upath");
-var mainRepo = "git@github.com:terrasync/main-test.git"
 
 
 var credCallback = function (url, userName) {
@@ -17,174 +16,218 @@ var committer = NodeGit.Signature.now("Groundweb",
 	"terrasync@github.com");
 
 
-module.exports.workflow = function (localPath, name, email, saveFunction, errCb, okCb) {
-	console.log("Cloning " + localPath);
+module.exports.workflow = function (localPath, icao, email, saveFunction, errCb, okCb, cloneURL) {
+	console.log("Cloning " + cloneURL + " into " + localPath);
+	try {
 
-	// Using the `clone` method from the `Git.Clone` module, bring down the
-	// NodeGit
-	// test repository from GitHub.
-	var cloneURL = "git@github.com:terrasync/main.git";
-	var myFetchOpts = {
-		callbacks: {
-			certificateCheck: function () { return 0; },
-			credentials: credCallback,
-			transferProgress: function (stats) {
-				try {
-					const progress = (100 * (stats.receivedObjects() + stats.indexedObjects())) / (stats.totalObjects() * 2);
-					console.log('progress: ', progress)
-				}
-				catch (Err) {
-					console.log('progress error: ' + Err)
+
+		// Using the `clone` method from the `Git.Clone` module, bring down the
+		// NodeGit
+		// test repository from GitHub.
+		var myFetchOpts = {
+			callbacks: {
+				certificateCheck: function () { return 0; },
+				credentials: credCallback,
+				transferProgress: function (stats) {
+					try {
+						const progress = (100 * (stats.receivedObjects() + stats.indexedObjects())) / (stats.totalObjects() * 2);
+						console.log('progress: ', progress)
+					}
+					catch (Err) {
+						console.log('progress error: ' + Err)
+					}
 				}
 			}
-		}
-	};
+		};
 
-	// Simple object to store clone options.
-	var cloneOptions = {};
+		// Simple object to store clone options.
+		var cloneOptions = {};
 
-	cloneOptions.checkoutBranch = "master";
+		cloneOptions.checkoutBranch = "master";
 
-	// This is a required callback for OS X machines. There is a known issue
-	// with libgit2 being able to verify certificates from GitHub.
-	cloneOptions.fetchOpts = myFetchOpts;
+		// This is a required callback for OS X machines. There is a known issue
+		// with libgit2 being able to verify certificates from GitHub.
+		cloneOptions.fetchOpts = myFetchOpts;
 
-	// Invoke the clone operation and store the returned Promise.
-	var cloneRepository = NodeGit.Clone(cloneURL, localPath, cloneOptions);
 
-	// If the repository already exists, the clone above will fail. You can
-	// simply
-	// open the repository in this case to continue execution.
-	var errorAndAttemptOpen = function (Err) {
-		console.log("Attempting to open " + Err);
-		return NodeGit.Repository.open(localPath);
-	};
+		// If the repository already exists, the clone above will fail. You can
+		// simply
+		// open the repository in this case to continue execution.
+		var errorAndAttemptOpen = function (err) {
+			console.log("Attempting to open ");
+			return NodeGit.Repository.open(localPath);
+		};
 
-	var branchName;
-	// Once the repository has been cloned or opened, you can work with a
-	// returned
-	// `Git.Repository` instance.
-	var repository;
-	cloneRepository.catch(errorAndAttemptOpen)
-		.then(function (repo) {
-			console.log("Got Repository");
-			// Access any repository methods here.
-			repository = repo;
-			return repo.fetchAll(myFetchOpts);
-		})
 
-		.then(function (err) {
-			console.log("Fetched " + err);
-			var mergeOptions = new NodeGit.MergeOptions();
-			return repository.mergeBranches("master", "origin/master", NodeGit.Signature.default(repository), NodeGit.Merge.PREFERENCE.NONE, mergeOptions)
-				.then(function (oid) {
-					console.log("Merged " + oid);
-					var addedPaths;
-					repository.getBranchCommit("master")
-						.then(function (oid) {
-							console.log("Got Master " + oid);
-							// create the branch
-							branchName = name + '_' + Date.now();
-							return NodeGit.Branch.create(repository, branchName, oid, 0);
-						}
-						)
-						.then(function (branch) {
-							console.log("Setting upstream " + branchName);
-							return NodeGit.Branch.setUpstream(branch, branchName);
-						})
-						.then(function (branch) {
-							var refspec = "refs/heads/" + branchName + ":refs/heads/" + branchName;
-							console.log("Add RefSpec " + refspec + "   " + branchName);
-							return NodeGit.Remote.addPush(repository, "origin", refspec);
-						})
-						.then(function (reference) {
-							console.log("Checking out branch " + "refs/heads/" + branchName);
-							return repository.checkoutBranch("refs/heads/" + branchName, {});
-						})
-						.then(function () {
-							return repository.getReferenceCommit(
-								"refs/heads/" + branchName);
-						})
-						.then(function (commit) {
-							console.log("Resetting to refs/heads/" + branchName + " " + commit);
-							return NodeGit.Reset.reset(repository, commit, NodeGit.Reset.TYPE.HARD, {});
-						})
-						// Let the file be saved
-						.then(function () { return saveFunction(); })
-						.then(function (paths) {
-							console.log("Refreshing index " + paths);
-							addedPaths = paths.map(p => path.relative(localPath, p));
-							addedPaths = addedPaths.map(p => upath.toUnix(p));
-							return repository.index();
-						})
-						.then(function (indexResult) {
-							index = indexResult;
-							addedPaths.forEach((element) => {
-								console.debug("Added : " + element);
-								index.addByPath(element).then(function (result) {
-									if (result != 0) {
-										console.error("Result " + result);
-									}
-									return index.write()
-								}).then(function (result) {
-									if (result != 0) {
-										console.error("Result " + result);
-									}
-								}
-								);
-							})
-							return index.write();
-						})
+		var branchName;
+		// Once the repository has been cloned or opened, you can work with a
+		// returned
+		// `Git.Repository` instance.
+		var repository;
+		NodeGit.Clone(cloneURL, localPath, cloneOptions)
+			.then(function (repo) {
+				console.log("Got Repository 1");
+				// Access any repository methods here.
+				repository = repo;
+				return repo.fetchAll(myFetchOpts);
+			}).catch(errorAndAttemptOpen)
+			.then(function (repo) {
+				console.log("Got Repository 2");
+				// Access any repository methods here.
+				repository = repo;
+				return repo.fetchAll(myFetchOpts);
+			})
 
-						.then(function (errCode) {
-							if (errCode != 0) {
-								console.error(errCode);
+			.then(function (err) {
+				if (err) {
+					errCb(err);
+				}
+				console.log("Fetched ");
+				var mergeOptions = new NodeGit.MergeOptions();
+				var defaultSignature = NodeGit.Signature.default(repository);
+				console.log(defaultSignature);
+				var result = repository.state();
+				if (result > NodeGit.Repository.STATE.NONE) {
+					console.log("Repostat " + result);
+					errCb("Repo not STATE.NONE");
+					return;
+				}
+
+				console.log("Repostat " + result);
+
+
+				repository.mergeBranches("master", "origin/master", defaultSignature, NodeGit.Merge.PREFERENCE.NONE, mergeOptions)
+					.then(function (oid) {
+						console.log("Merged " + oid);
+						var addedPaths;
+						repository.getBranchCommit("master")
+							.then(function (oid) {
+								console.log("Got Master " + oid);
+								// create the branch
+								branchName = icao + '_' + Date.now();
+								return NodeGit.Branch.create(repository, branchName, oid, 0);
 							}
-							return index.writeTree();
-						})
-						.then((oidResult) => {
-							treeOid = oidResult;
-							return NodeGit.Reference.nameToId(repository, "HEAD");
-						})
-						.then(function (head) {
-							console.log("Got Head " + head);
-							return repository.getCommit(head);
-						})
-						.then(function (parent) {
-							console.log("Oid " + treeOid);
-							console.log("Parent Head Commit " + parent);
+							)
+							.then(function (branch) {
+								console.log("Setting upstream " + branchName);
+								return NodeGit.Branch.setUpstream(branch, branchName);
+							})
+							.then(function (branch) {
+								var refspec = "refs/heads/" + branchName + ":refs/heads/" + branchName;
+								console.log("Add RefSpec " + refspec + "   " + branchName);
+								return NodeGit.Remote.addPush(repository, "origin", refspec);
+							})
+							.then(function (reference) {
+								console.log("Checking out branch " + "refs/heads/" + branchName);
+								return repository.checkoutBranch("refs/heads/" + branchName, {});
+							})
+							.then(function () {
+								return repository.getReferenceCommit(
+									"refs/heads/" + branchName);
+							})
+							.then(function (commit) {
+								console.log("Resetting to refs/heads/" + branchName + " " + commit);
+								return NodeGit.Reset.reset(repository, commit, NodeGit.Reset.TYPE.HARD, {});
+							})
+							// Let the file be saved (Callback)
+							.then(function () { return saveFunction(); })
+							.then(function (paths) {
+								if (!paths)
+									errCb("saveFunction() returns undefined");
+								console.log("Refreshing index " + paths);
+								addedPaths = paths.map(p => path.relative(localPath, p));
+								addedPaths = addedPaths.map(p => upath.toUnix(p));
+								return repository.index();
+							})
+							.then(function (indexResult) {
+								index = indexResult;
+								addedPaths.forEach((element) => {
+									console.debug("Added : " + element);
+									index.addByPath(element).then(function (result) {
+										if (result != 0) {
+											console.error("Result " + result);
+										}
+										return index.write()
+									}).then(function (result) {
+										if (result != 0) {
+											console.error("Result " + result);
+										}
+									}
+									);
+								})
+								return index.write();
+							})
 
-							var author = NodeGit.Signature.now(email,
-								email);
-							console.log("Committing to refs/heads/" + branchName);
-							return repository.createCommit("HEAD", author, committer, "New Groundnet for " + name,
-								treeOid, [parent]);
-						})
-						.then(function (commitOid) {
-							console.log("Committed " + commitOid);
-							console.log("Switching back to master");
-							repository.checkoutBranch("master", {});
-							return repository;
-						})
-						.then(function () {
-							return NodeGit.Remote.lookup(repository, "origin");
-						})
-						.then(function (remote) {
-							var refspec = "refs/heads/" + branchName + ":refs/heads/" + branchName;
-							console.log("Pushing " + refspec);
-							return remote.push(
-								["+" + refspec],
-								myFetchOpts
-								, committer, "Push to " + refspec
-							);
-						})
-						.then(function () {
-							console.log(branchName);
-							okCb(branchName);
-						})
-						.catch(errCb);
-				}).catch(errCb);
-		}).catch(errCb);
+							.then(function (errCode) {
+								if (errCode != 0) {
+									console.error(errCode);
+								}
+								return index.writeTree();
+							})
+							.then((oidResult) => {
+								treeOid = oidResult;
+								return NodeGit.Reference.nameToId(repository, "HEAD");
+							})
+							.then(function (head) {
+								console.log("Got Head " + head);
+								return repository.getCommit(head);
+							})
+							.then(async function (parent) {
+								const diff = await NodeGit.Diff.treeToIndex(repository, await parent.getTree(), null);
+								const patches = await diff.patches();
+								if (patches.length < 5) {
+									var err = new Error("No difference detected. Is the file different? ");
+									//errCb(err);
+									throw err;
+								}
+								console.log(patches.map((patch) => patch.newFile().path()));
+								return parent;
+							})
+							.then(function (parent) {
+								console.log("Oid " + treeOid);
+								console.log("Parent Head Commit " + parent);
+
+								var author = NodeGit.Signature.now(email,
+									email);
+								console.log("Committing to refs/heads/" + branchName);
+								return repository.createCommit("HEAD", author, committer, "New Groundnet for " + icao,
+									treeOid, [parent]);
+							})
+							.then(function (commitOid) {
+								console.log("Committed " + commitOid);
+								console.log("Switching back to master");
+								repository.checkoutBranch("master", {});
+								return repository;
+							})
+							.then(function () {
+								return NodeGit.Remote.lookup(repository, "origin");
+							})
+							.then(function (remote) {
+								var refspec = "refs/heads/" + branchName + ":refs/heads/" + branchName;
+								console.log("Pushing " + refspec);
+								return exports.push(remote, refspec, myFetchOpts, committer);
+							})
+							.then(function () {
+								console.log(branchName);
+								okCb(branchName);
+							})
+							.catch(errCb);
+					}).catch(errCb);
+
+			}).catch(errCb);
+	} catch (err) {
+		errCb(err);
+	}
+}
+
+/**Externalized for testing purpose (stubbing) */
+
+module.exports.push = function (remote, refspec, myFetchOpts, committer) {
+	return remote.push(
+		["+" + refspec],
+		myFetchOpts
+		, committer, "Push to " + refspec
+	);
 }
 
 module.exports.commit = function (name) {
---
+++ test/test.js
@@ -0,0 +1,147 @@
+var chai = require('chai');
+var assert = require('chai').assert;
+const path = require("path");
+const fs = require('fs');
+var dirindex = require('../api/util/dirindex.js');
+const sinon = require('sinon');
+var sandbox = require('sinon').createSandbox();
+
+var NodeGit = require("nodegit");
+
+
+const terraSyncDir = 'public_test';
+const upstreamDir = 'public_upstream';
+
+
+var git = require('../api/util/git.js');
+var github = require('../api/util/github.js');
+
+describe('Array', function () {
+  it('git test no diff', (done) => {
+
+    var mock = sinon.mock(NodeGit);
+
+    var extract = require('extract-zip')
+    var localPath = path.resolve(path.join(terraSyncDir, "/main/"));
+    createPath(localPath);
+    var upstreamPath = path.resolve(path.join(upstreamDir, "/main/"));
+    extract('test/test-repo.zip', { dir: upstreamPath }, function (err) {
+      if (err)
+        done(err);
+      console.log("Unpacked into " + upstreamPath);
+      var name = 'Tom Tester';
+      var email = 'tom@example.org';
+      var saveFunction = () => {
+        console.log("Copy File ");
+        // destination.txt will be created or overwritten by default.
+        var paths = [];
+        fs.copyFileSync('test/EDDP.groundnet.xml', path.join(localPath, '/E/D/D/EDDP.groundnet.xml'));
+        if (err)
+          done(err);
+        console.log('EDDP.groundnet.xml was copied to EDDP.groundnet.xml');
+        var currentpath = path.join(localPath, '/E/D/D/');
+        paths.push(path.join(currentpath, 'EDDP.groundnet.xml'));
+        do {
+          paths.push(dirindex.buildDirIndex(currentpath));
+        }
+        while ((currentpath = path.resolve(currentpath, "..")) != path.resolve(terraSyncDir))
+        return paths;
+      };
+      var errCb = (err) => {
+        //      console.log(err);
+        //      assert.fail(err);
+        if (!localPath)
+          fs.rmdirSync(localPath);
+        if (!upstreamPath)
+          fs.rmdirSync(upstreamPath);
+        done();
+      };
+      var okCb = () => {
+        if (!localPath)
+          fs.rmdirSync(localPath);
+        if (!upstreamPath)
+          fs.rmdirSync(upstreamPath);
+        done("Shouldn't be OK");
+      };
+      var cloneURL;
+      var stub = sandbox.replace(git, "push", function (remote, refspec, myFetchOpts, committer) {
+        console.log("PUSH " + committer);
+        done("PUSCHED");
+      });
+      git.workflow(localPath, 'EDDP', email, saveFunction, errCb, okCb, "file:///" + upstreamPath);
+      //sandbox.assert.calledOnce(stub);
+      sandbox.restore();
+    })
+
+    //    createPath(gitPath);
+  }).timeout(60000);
+});
+describe('Git test with diff', function () {
+  it('git test with diff', (done) => {
+
+    var mock = sinon.mock(NodeGit);
+
+    var extract = require('extract-zip')
+    var localPath = path.resolve(path.join(terraSyncDir, "/main/"));
+    createPath(localPath);
+    var upstreamPath = path.resolve(path.join(upstreamDir, "/main/"));
+    extract('test/test-repo.zip', { dir: upstreamPath }, function (err) {
+      if (err)
+        done(err);
+      console.log("Unpacked into " + upstreamPath);
+      var name = 'Tom Tester';
+      var email = 'tom@example.org';
+      var saveFunction = () => {
+        console.log("Copy File ");
+        // destination.txt will be created or overwritten by default.
+        var paths = [];
+        fs.copyFileSync('test/EDDP.groundnet.1.xml', path.join(localPath, '/E/D/D/EDDP.groundnet.xml'));
+        if (err)
+          done(err);
+        console.log('EDDP.groundnet.xml was copied to EDDP.groundnet.xml');
+        var currentpath = path.join(localPath, '/E/D/D/');
+        paths.push(path.join(currentpath, 'EDDP.groundnet.xml'));
+        do {
+          paths.push(dirindex.buildDirIndex(currentpath));
+        }
+        while ((currentpath = path.resolve(currentpath, "..")) != path.resolve(terraSyncDir))
+        return paths;
+      };
+      var errCb = (err) => {
+        //      console.log(err);
+        //      assert.fail(err);
+        if (!localPath)
+          fs.rmdirSync(localPath);
+        if (!upstreamPath)
+          fs.rmdirSync(upstreamPath);
+        done(err);
+      };
+      var okCb = () => {
+        if (!localPath)
+          fs.rmdirSync(localPath);
+        if (!upstreamPath)
+          fs.rmdirSync(upstreamPath);
+        done();
+      };
+      var cloneURL;
+      var stub = sandbox.replace(git, "push", function (remote, refspec, myFetchOpts, committer) {
+        console.log("PUSH " + committer);
+      });
+      git.workflow(localPath, 'EDDP', email, saveFunction, errCb, okCb, "file:///" + upstreamPath);
+      //sandbox.assert.calledOnce(stub);
+
+    })
+    //    createPath(gitPath);
+  }).timeout(60000);
+
+});
+
+
+function createPath(currentpath) {
+  if (fs.existsSync(currentpath))
+    return;
+  fs.mkdirSync(currentpath, { recursive: true }, (err) => {
+    console.error('Error creating path', err);
+    return;
+  });
+}
